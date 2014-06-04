495502cc636dd3493564a0327e5c9851d32e0e1f
PFei_He <user@userdeMac-mini-2.local>
2014-06-04 15:30:30 +0800

    Initial Commit

---
+++ PFShareKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PFShareKit
+//
+//  Created by PFei_He on 14-6-4.
+//  Copyright (c) 2014年 PFei_He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
